6741262d64559a5e0ec720d3ab0a178933a3769a
aseem <aseem.tim@gmail.com>
2022-06-02 19:44:41 -0400

    unicafe 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,38 @@
 import { useState } from 'react'
 
-const App = () => {
+const Heading = (props) => <h1>{props.text}</h1>
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+const Statistics = ({ text, number }) => {
   return (
-    <div>Hello world</div>
+    <div>
+      {text} {number}
+    </div>
   )
+} 
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <Heading text={"give feedback"} />
+      <Button text={"good"} handleClick={handleGood} />
+      <Button text={"neutral"} handleClick={handleNeutral} />
+      <Button text={"bad"} handleClick={handleBad} />
+      <Heading text={"statistics"} />
+      <Statistics text={"good"} number={good} />
+      <Statistics text={"neutral"} number={neutral} />
+      <Statistics text={"bad"} number={bad} />
+    </div>
+    )
 }
 
 export default App;
