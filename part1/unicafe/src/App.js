4fe83af01fe611473800d284498fbf980a9fbb58
aseem <aseem.tim@gmail.com>
2022-06-02 22:11:23 -0400

    unicafe 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
   const handleGood = () => setGood(good + 1)
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
@@ -31,6 +31,14 @@ const App = () => {
       <Statistics text={"good"} number={good} />
       <Statistics text={"neutral"} number={neutral} />
       <Statistics text={"bad"} number={bad} />
+      <Statistics text={"all"} number={good + neutral + bad} />
+      <Statistics 
+        text={"average"} 
+        number={(good - bad) / (good + neutral + bad)} />
+      <Statistics 
+        text={"positive"} 
+        number={(good/(good+neutral+bad))*100+" %"} 
+      />
     </div>
     )
 }
