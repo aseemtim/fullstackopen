ede0dc21b7f743f8bd47091c7db749d421f2ad46
aseem <aseem.tim@gmail.com>
2022-06-14 22:53:40 -0400

    phonebook 2.12 implemented form

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,8 +1,19 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const App = () => {
+  // do this next
+  const [countries, setCountries] = useState([])
+  const [countryToSearch, setCountryToSearch] = useState('')
+
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+    setCountryToSearch(event.target.value)
+  }
+
   return (
     <div>
-      Hello world :)
+      find countries <input onChange={handleSearch} value={countryToSearch}/>
     </div>
   );
 }
